624494885919f36f12ee7236a809d4f39f3d563f
Dharmen Shah <dharmen.s@milestoneinternet.com>
2021-01-20 16:59:15 +0530

    refactor: 💡 move template and css in their own files

--- projects/ngneat/hot-toast/src/lib/components/animated-icon/animated-icon.component.ts
+++ projects/ngneat/hot-toast/src/lib/components/animated-icon/animated-icon.component.ts
@@ -2,33 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hot-toast-animated-icon',
-  template: `
-    <div class="hot-toast-animated-icon">
-      <ng-content></ng-content>
-    </div>
-  `,
-  styles: [
-    `
-      @keyframes enter {
-        from {
-          transform: scale(0.6);
-          opacity: 0.4;
-        }
-
-        to {
-          transform: scale(1);
-          opacity: 1;
-        }
-      }
-
-      .hot-toast-animated-icon {
-        position: relative;
-        transform: scale(0.6);
-        opacity: 0.4;
-        min-width: 20px;
-        animation: enter 0.3s 0.12s cubic-bezier(0.175, 0.885, 0.32, 1.275) forwards;
-      }
-    `,
-  ],
+  templateUrl: './animated-icon.component.html',
+  styleUrls: ['./animated-icon.component.scss'],
 })
 export class AnimatedIconComponent {}
--- projects/ngneat/hot-toast/src/lib/components/indicator/icons/loader/loader.component.ts
+++ projects/ngneat/hot-toast/src/lib/components/indicator/icons/loader/loader.component.ts
@@ -3,33 +3,8 @@ import { IconTheme } from '../../../../hot-toast.model';
 
 @Component({
   selector: 'hot-toast-loader',
-  template: `<div
-    class="hot-toast-loader-icon"
-    [ngStyle]="{ 'border-color': theme?.primary || '#e0e0e0', 'border-right-color': theme?.secondary || '#616161' }"
-  ></div> `,
-  styles: [
-    `
-      @keyframes rotate {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      }
-
-      .hot-toast-loader-icon {
-        width: 12px;
-        height: 12px;
-        box-sizing: border-box;
-        border: 2px solid;
-        border-radius: 100%;
-        border-color: #e0e0e0;
-        border-right-color: #616161;
-        animation: rotate 1s linear infinite;
-      }
-    `,
-  ],
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss'],
 })
 export class LoaderComponent {
   @Input() theme: IconTheme;
