8cc9b2732aa9ce848e23efa258597ab4425ef852
Dharmen Shah <dharmen.s@milestoneinternet.com>
2021-01-23 22:46:51 +0530

    feat: 🎸 schematics

---
+++ projects/ngneat/hot-toast/schematics/ng-add/utils/project-targets.ts
@@ -0,0 +1,19 @@
+import { SchematicsException } from '@angular-devkit/schematics';
+import { WorkspaceProject } from '@schematics/angular/utility/workspace-models';
+
+/** Resolves the architect options for the build target of the given project. */
+export function getProjectTargetOptions(project: WorkspaceProject, buildTarget: string) {
+  if (project.targets && project.targets[buildTarget] && project.targets[buildTarget].options) {
+    return project.targets[buildTarget].options;
+  }
+
+  if (project.architect && project.architect[buildTarget] && project.architect[buildTarget].options) {
+    return project.architect[buildTarget].options;
+  }
+
+  throw new Error(`Cannot determine project target configuration for: ${buildTarget}.`);
+}
+
+export function targetBuildNotFoundError(): SchematicsException {
+  return new SchematicsException(`Project target "build" not found.`);
+}
